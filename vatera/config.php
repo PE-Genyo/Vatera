a426c4fe277b408fa1cab4d97b99ac42c14257f5
PE-Genyo <horvath.bence.sk@gmail.com>
2020-04-07 12:49:39 +0200

    Clean

--- vatera/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define('DB_SERVER','localhost');
-	define('DB_USERNAME','vatera');
-	define('DB_PASSWORD','vatera123');
-	define('DB_DATABASE','vatera');
-
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
