8020ca01943e8ce569f65e17fed8f27b20b040c4
varazslo20 <62426571+varazslo20@users.noreply.github.com>
2020-04-29 02:10:42 +0200

    frontend + backend osszemergelve :)

---
+++ RendszerFejlMergelt/addItem.php
@@ -0,0 +1,127 @@
+<html>
+<head>
+<title> add item </title>
+<style type="text/css">
+body {
+    background-image: url("backgroundimg.jpg");
+    background-size: cover;
+}
+
+h1 {
+  text-align: center;
+}
+
+hr {
+    width: 80%;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+  text-align: center;
+}
+
+table {
+    text-align: center;
+    color:black;
+}
+
+#table1 {
+    border-collapse: separate;
+    border-spacing: 15px;
+}
+</style>
+</head>
+<body>
+
+<?php
+session_start();
+include_once 'class.user.php'; $user = new User();
+
+
+if (isset($_REQUEST['submit'])){
+    extract($_REQUEST);
+    $addItem = $user->addItem($nev, $mennyiseg,$ar, $idopont, $licitkulonbseg, $aktualislicit, $leiras, $_SESSION['uid']);
+    if ($addItem) {
+        // Registration Success
+        echo 'Sikeres felvitel!';
+        header("location:home.php");
+    } else {
+        // Registration Failed
+        echo 'Sikertelen felvitel';
+    }
+}
+include("footer.php");
+?>
+
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<script type="text/javascript" language="javascript">
+
+    function checkDatas() {
+        var form = document.addItem;
+        console.log(form.ar.value);
+        console.log(form.aktualislicit.value);
+
+        if(form.aktualislicit.value > form.ar.value){
+            alert( "A licit nem lehet nagyobb mint az ár" );
+            return false;
+        }
+
+        if ( (Date.parse(form.idopont.value)) - (Date.parse(new Date().toDateString())) < 0){
+            alert( "Az időtartam nem lehet kisebb mint a mostani dátum" );
+            return false;
+        }
+    }
+
+</script>
+
+<div id="container">
+    <h1 style="color: black;">Itt tudsz hirdetést feladni</h1>
+    <hr>
+    <form action="" method="post" name="addItem">
+        <table align = "center" id = "table1">
+            <tbody>
+            <tr>
+                <th>Név:</th>
+                <td><input type="text" name="nev" required="" /></td>
+            </tr>
+            <tr>
+                <th>Mennyiség:</th>
+                <td><input type="number" min="0" name="mennyiseg" required="" /></td>
+
+            </tr>
+            <tr>
+                <th>Ár:</th>
+                <td><input type="number" min="1" placeholder="Az ár Ft-ban értendő" name="ar" required="" /></td>
+
+            </tr>
+            <tr>
+                <th>Időtartam:</th>
+                <td><input type="date" name="idopont"  required="" /></td>
+
+            </tr>
+            <tr>
+                <th>Min. licit különbség:</th>
+                <td><input type="number" name="licitkulonbseg" required="" /></td>
+            </tr>
+            <tr>
+                <th>Licit kezdoertek:</th>
+                <td><input type="number" min="0" name="aktualislicit" required="" /></td>
+            </tr>
+            <tr>
+                <th>Leiras:</th>
+                <td><textarea name="leiras" required="" placeholder="Ide ird a termek leirast"></textarea></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input onclick="return(checkDatas()); " type="submit" name="submit" value="Felvitel" /></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><a href="home.php">Vissza</a></td>
+            </tr>
+            </tbody>
+        </table>
+    </form></div>
+</body>
+</html>
